536a6eba7de642702ced38b6bd5e1b0bbaf5b1dc
rmoczarski <rmoczarski@kartgis.com.pl>
2020-11-17 22:13:06 +0100

    Methods implemented

--- src/com/company/MyStructure.java
+++ src/com/company/MyStructure.java
@@ -1,29 +1,82 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyStructure implements IMyStructure {
-    private List<INode> nodes;
+    private List<INode> nodes = new ArrayList<>();
+
 
 
     @Override
     public INode findByCode(String code) {
         for(INode node: nodes){
-            if(node.getCode().equals(code)){
-                return node;
+            if (node instanceof ICompositeNode) {
+                List<INode> flatModelNodes = getFlatModel((((ICompositeNode) node).getNodes()));
+                for (INode iNodeFromFlatModel : flatModelNodes) {
+                    if (iNodeFromFlatModel.getCode().equals(code)) {
+                        return iNodeFromFlatModel;
+                    }
+                }
+            } else {
+                if (node.getCode().equals(code)) {
+                    return node;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public INode findByRenderer(String renderer) {
+        for (INode node : nodes) {
+            if (node instanceof ICompositeNode) {
+                List<INode> flatModelNodes = getFlatModel((((ICompositeNode) node).getNodes()));
+                for (INode iNodeFromFlatModel : flatModelNodes) {
+                    if (iNodeFromFlatModel.getRenderer().equals(renderer)) {
+                        return iNodeFromFlatModel;
+                    }
+                }
+            } else {
+                if (node.getRenderer().equals(renderer)) {
+                    return node;
+                }
             }
         }
         return null;
     }
 
     @Override
-    public INode findByRenderer(String renderer) {
-        return null;
+    public int count() {
+        List<INode> allNodes = new ArrayList<>();
+        for (INode node : nodes) {
+            if (node instanceof ICompositeNode) {
+                List<INode> flatModelNodes = getFlatModel((((ICompositeNode) node).getNodes()));
+                allNodes.addAll(flatModelNodes);
+            } else {
+                allNodes.add(node);
+            }
+        }
+        return allNodes.size();
     }
 
-    @Override
-    public int count() {
-        return 0;
+    private List<INode> getFlatModel(List<INode> iNodeList) {
+        Boolean isCompositeNodePresent = false;
+        for (INode iNode : iNodeList) {
+            if (!(iNode instanceof ICompositeNode)) {
+                iNodeList.add(iNode);
+            } else {
+                isCompositeNodePresent = true;
+                iNodeList.addAll(((ICompositeNode) iNode).getNodes());
+            }
+        }
+        if (isCompositeNodePresent) {
+            return getFlatModel(iNodeList);
+        } else {
+            return iNodeList;
+
+        }
     }
 }
 
